3317a5d797899ed65685da60ff241a6b216ff51d
Eben <maradebenezer@gmail.com>
2023-05-09 13:26:51 +0100

    completed console prototype

--- console.py
+++ console.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" script implementing an interactive console for the
-    AirBnB_clone """
+""" script implementing an interactive console for the AirBnB_clone """
 import cmd
 import sys
 
@@ -8,19 +7,19 @@ import sys
 class AirBnB_shell(cmd.Cmd):
     """The AirBnB shell interpreter, based on cmd module"""
     prompt = "(hbnb) "
-
-    def __init__(self, completekey='tab', stdin=None, stdout=None):
-        """ the init fnction """
-        super().__init__(completekey=completekey, stdin=stdin, stdout=stdout)
-        pass
     
     def do_quit(self, arg):
-        """ quit shell """
-        return True
+        """ implement the quit """
+        return (True)
     
     def do_EOF(self, arg):
-        """ respond to EOF """
-        return True
+        """ implement EOF """
+        print("")
+        return (True)
+
+    def emptyline(self):
+        """ prevent emptyline by overriding """
+        pass
 
 
 AirBnB_shell().cmdloop()
