fa27b0793f4fd61546ba1cc273ae679f5c24f18b
Eben <maradebenezer@gmail.com>
2023-05-09 05:57:45 +0100

    implemented shell prototype

--- console.py
+++ console.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ script implementing an interactive console for the
     AirBnB_clone """
-import cmd, sys
+import cmd
+import sys
 
 
 class AirBnB_shell(cmd.Cmd):
     """The AirBnB shell interpreter, based on cmd module"""
     prompt = "(hbnb) "
     file = None
+
 
 AirBnB_shell().cmdloop()
