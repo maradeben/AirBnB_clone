6229d77fad3a85cca7301656ae90738c6e4938a1
Eben <maradebenezer@gmail.com>
2023-05-09 07:53:58 +0100

    finished shell interaction prototype

--- console.py
+++ console.py
@@ -8,7 +8,19 @@ import sys
 class AirBnB_shell(cmd.Cmd):
     """The AirBnB shell interpreter, based on cmd module"""
     prompt = "(hbnb) "
-    file = None
+
+    def __init__(self, completekey='tab', stdin=None, stdout=None):
+        """ the init fnction """
+        super().__init__(completekey=completekey, stdin=stdin, stdout=stdout)
+        pass
+    
+    def do_quit(self, arg):
+        """ quit shell """
+        return True
+    
+    def do_EOF(self, arg):
+        """ respond to EOF """
+        return True
 
 
 AirBnB_shell().cmdloop()
